f89ca7173afb09d841c2c7b121d229bcd9c7085a
GBossetti <gonzalo.bossetti@gmail.com>
2023-03-08 09:15:42 -0300

    Se agrega Script para crear la BD

---
+++ Script/MasterClassBD.sql
@@ -0,0 +1,17 @@
+create database masterclass_f5
+go
+
+use masterclass_f5
+go
+
+create table BandasMusicales (
+	id int primary key identity(1,1),
+	Banda varchar(200) not null,
+	Coder varchar(100) not null
+)
+
+insert into BandasMusicales (Banda, COder)
+values ('Patricio Rey y sus Redonditos de Ricota', 'Gonzalo')
+
+select * 
+from BandasMusicales
